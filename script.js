4e34ad395e5960e3ac623e67a295b1d999a90290
Alex Ha <haalex1101@gmail.com>
2025-12-29 16:55:17 -0500

    Create function for compuer choice.

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-function getComputerChoice() {
-    
-}
-
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function getComputerChoice() {
+    let int = getRandomInt(1,3);
+    let choice = null;
 
+    switch (int) {
+        case 1: 
+            choice = "rock"
+            break;
+        case 2:
+            choice = "paper"
+            break;
+        case 3:
+            choice = "scissors"
+            break;
+        default:
+            choice = "hello"
+    }
+
+    return choice;
+}
   
-
+for (let i = 0; i < 100; i++) {
+    console.log(getComputerChoice())
+}
